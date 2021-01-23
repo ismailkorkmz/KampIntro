6f00242f1ddd8b9c2d01c06d88c089d2726c1a18
ismailmurat korkmaz <348887@ogr.ktu.edu.tr>
2021-01-24 00:18:58 +0300

    23 Ocak Ders Sonu

---
+++ Oop2/MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    // İş sınıfları // business clases
+    class MusteriManager // CustomerManager-Operasyon Sınıfı
+    {
+        public void Ekle(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+        }
+    }
+}
